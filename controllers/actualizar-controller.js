d6201495fad4dff16a3771e254f982ca502eadfa
estebanvasquex <estebanvasquex@gmail.com>
2023-02-09 21:59:13 -0500

    Uso de async -await y try catch, Se modifica la función obtenerInformación y se agrega async y await, con esto eliminamos los .then y damos paso a la manipulación de errores, se agrega el uso de try catch para el trabajo con errores y se valida con un 'if' en caso de que no llegue una correcta respuesta de la petición

--- controllers/actualizar-controller.js
+++ controllers/actualizar-controller.js
@@ -1,26 +1,39 @@
 import { clientServices } from "../service/client-service.js";
 
 
-const formulario = document.querySelector("[data-form]")
-console.log(formulario);
+const formulario = document.querySelector("[data-form]");
 
 
-const obtenerInformación = () =>{
-const url = new URL(window.location);
-/* con esto puedo capturar una url y a la url se le agregó un params desde el archivo client-controller.js con el nombre "id" href="../screens/editar_cliente.html?id=${id} ahora puedo utilizar ese id para hacer la consulta */
-const id = url.searchParams.get("id");
-/* valida la url, si lleva el params y ni no es así lo que hace es enviar la página de error */
-/* if(id===null  || id === ""){
-    console.log("error de id: " + id);
-    window.location.href = "/screens/error.html";
-} */
-const nombre = document.querySelector("[data-nombre]");
-const email = document.querySelector("[data-email]");
-/* si todo sale bien la respuesta es un json y se le da el nombre de perfil y se imprime en consola */
-clientServices.detalleCliente(id).then((perfil) =>{
-    nombre.value = perfil.nombre;
-    email.value = perfil.email;
-});
+const obtenerInformación = async () =>{
+    const url = new URL(window.location);
+    /* con esto puedo capturar una url y a la url se le agregó un params desde el archivo client-controller.js con el nombre "id" href="../screens/editar_cliente.html?id=${id} ahora puedo utilizar ese id para hacer la consulta */
+    const id = url.searchParams.get("id");
+    /* valida la url, si lleva el params y ni no es así lo que hace es enviar la página de error */
+    /* if(id===null  || id === ""){
+        console.log("error de id: " + id);
+        window.location.href = "/screens/error.html";
+    } */
+    const nombre = document.querySelector("[data-nombre]");
+    const email = document.querySelector("[data-email]");
+
+    try {
+         //consulta el usuario con el id en bd y los devuelve en los input
+        const perfil = await clientServices.detalleCliente(id)
+        //se valida si la peticiín cuenta con una respuesta y en caso de que no, se procede a manipular el error
+        if (perfil.nombre && perfil.email) {
+                nombre.value = perfil.nombre;
+                email.value = perfil.email;
+        } else {
+            throw new Error();
+        }
+      
+    } catch (error) {
+        console.log("Catch Error: ", error);
+        alert("Hubo un error con los datos");
+        window.location.href = "/screens/error.html"
+        
+    }
+
 };
 
 obtenerInformación()
